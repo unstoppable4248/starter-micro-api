5fcb80accdfc9ae3d3b319a38f378a07d3deccd9
unstoppable <unstoppable4248@gmail.com>
2022-12-16 22:46:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let express = require("express");
+let axios = require("axios");
+let jwt = require("jsonwebtoken");
+require("dotenv").config();
+let PORT = process.env.PORT || 3000;
+let app = express();
+app.use(express.json());
+
+app.post("/signin", (req, res) => {
+  axios.get("https://6393561fab513e12c50b63b8.mockapi.io/api/wallet").then(function (response) {
+    let users = response.data;
+    for (let i of users) {
+      if (i.username == req.body.username || i.email == req.body.username) {
+        const token = jwt.sign({ username: i.username }, process.env.JWT_KEY, {
+          expiresIn: 900000,
+        });
+        res.status(200).json({ token });
+        return;
+      }
+    }
+    res.status(404).send("Username does not exist.");
+  });
+});
+
+app.post("/verify", (req, res) => {
+  jwt.verify(req.body.jwt, process.env.JWT_KEY, (err, resp) => {
+    if (err) return res.status(401).send(err);
+    return res.send("Successfully Verified");
+  });
+});
+
+app.listen(PORT);
